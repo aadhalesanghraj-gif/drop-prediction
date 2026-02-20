e89d5796c2633403c5c47aaec64270e9b5257902
om <omjaigude161@gmail.com>
2026-02-20 10:15:49 +0530

    flask made

---
+++ Backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+
+template_dir = os.path.abspath('./frontend')
+
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
